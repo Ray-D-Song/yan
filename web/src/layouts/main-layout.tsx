e9abf39128fb8abd7b08a0e3f4a22bff57498869
Ray-D-Song <songraysmail@gmail.com>
2025-12-21 19:29:15 +0800

    feat: main layout

---
+++ web/src/layouts/main-layout.tsx
@@ -0,0 +1,50 @@
+import { defineComponent } from 'vue'
+import SidebarLeft from '@/components/SidebarLeft.vue'
+import SidebarRight from '@/components/SidebarRight.vue'
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+} from '@/components/ui/breadcrumb'
+import { Separator } from '@/components/ui/separator'
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from '@/components/ui/sidebar'
+
+export default defineComponent({
+  name: 'MainLayout',
+  setup(_, { slots }) {
+    return () => (
+      <SidebarProvider>
+        <SidebarLeft />
+        <SidebarInset>
+          <header class="sticky top-0 flex h-14 shrink-0 items-center gap-2 bg-background">
+            <div class="flex flex-1 items-center gap-2 px-3">
+              <SidebarTrigger />
+              <Separator
+                orientation="vertical"
+                class="mr-2 data-[orientation=vertical]:h-4"
+              />
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem>
+                    <BreadcrumbPage class="line-clamp-1">
+                      Project Management & Task Tracking
+                    </BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
+          </header>
+          <div class="flex flex-1 flex-col gap-4 p-4">
+            {slots.default?.()}
+          </div>
+        </SidebarInset>
+        <SidebarRight />
+      </SidebarProvider>
+    )
+  },
+})
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,3 +1,4 @@
+import mainLayout from '@/layouts/main-layout'
 import login from '@/pages/login'
 import signup from '@/pages/signup'
 import { createRouter, createWebHistory } from 'vue-router'
@@ -15,6 +16,10 @@ const router = createRouter({
         {
           path: '/signup',
           component: signup
+        },
+        {
+          path: '/main',
+          component: mainLayout
         }
       ]
     }
